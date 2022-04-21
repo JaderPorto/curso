6070b4bd29c489681ae485ee235d25d1a15760ae
JaderPorto <maxijader@gmail.com>
2022-04-21 10:28:21 -0300

    curso

---
+++ Fundamentos/funcoes/classe_vc_factory.js
@@ -0,0 +1,19 @@
+class pessoa{
+    constructor(nome){
+        this.nome = nome
+    }
+    falar(){
+        console.log(`meu nome e ${this.nome}`)
+    }
+    
+}
+const p1 = new pessoa('Joao')
+p1.falar()
+
+const criarPessoa = nome =>{
+    return{
+        falar:()=>console.log(`meu nome e ${nome}`)
+    }
+}
+const p2 = criarPessoa('Joao')
+p2.falar
