0e4bfebe95a5b6e44d6dcf92f1d7765ad287cce1
JaderPorto <maxijader@gmail.com>
2022-04-25 06:21:01 -0300

    curso

---
+++ objeto/objetoRevisao.js
@@ -0,0 +1,34 @@
+// colecao dinamica de pares chave/valor
+const produto = new Object
+produto.nome = 'Cadeira'
+produto['marca do produto'] = 'Generica'
+produto.preco =220
+
+console.log(produto)
+
+const carro = {
+    modelo:'A4',
+    valor:89000,
+    proprietario:{
+        nome:'Raul',
+        idade:56,
+        endereco:{
+            lagradouro:'Rua ABC',
+            numero:123
+        }
+    },
+    condutores:[{
+        nome: 'Junior',
+        idade: 19
+        },{ 
+          nome:'Ana',
+          idade:42
+    }],
+    calcularValorSeguro:function(){
+        //...
+    }
+}
+
+carro.proprietario.endereco.numero =1000
+carro['proprietario'] ['endereco'] ['lagradouro'] = 'Av Gigante'
+console.log(carro)
